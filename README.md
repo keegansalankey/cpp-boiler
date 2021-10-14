a6d2543a9daf4806d877f721312cf9a96dcd1159
Keegan Salankey <keegansalankey@gmail.com>
2021-10-14 02:32:54 -0600

    update readme and added better functionality for file systems

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# cpp-boiler
+#cpp-boiler
+This project is currently waiting to be further expanded/revised, but basic functionality is possible.
 
-This project is currently under construction
+##Usage
+I currently have this project stored in my `$HOME` directory (most likely only functional on linux machines for the time being).
+I then have an executable in `$HOME/bin/` directory titled `boiler-cpp`. The contents are as follows:
+    #!/bin/bash
+    python3 ~/cpp-boiler/make_project.py $@
+
+
--- make_project.py
+++ make_project.py
@@ -13,9 +13,23 @@ if __name__ == "__main__":
         exit(1)
     
     template_type, project_name = argv[1], argv[2]
+    
+    ####################################################################################################
+    #
+    # Thoughts on how to make this work outside of the cpp-boiler directory:
+    # 
+    #   1) Store working directory in variable to know where to create project
+    #   2) Get home directory path and then append path to the cpp-boiler directory
+    #   3) Change directory to home directory
+    #   3) Should be able to copy between those two
+    #
+    ####################################################################################################
+    
+    boiler_path = os.path.dirname(os.path.realpath(__file__))
+    target_path = os.getcwd() + '/'
 
     # verify that template type exists by scanning through directory
-    with os.scandir("templates/") as templates:
+    with os.scandir(boiler_path + "/templates/") as templates:
         for template in templates:
             if template.is_dir() and template.name == template_type:
                 template_type += "/"
@@ -28,8 +42,8 @@ if __name__ == "__main__":
         exit(1)
                 
     
-    template_path = "templates/" + template_type
-    project_path = "./" + project_name + "/"
+    template_path = boiler_path + "/templates/" + template_type
+    project_path = target_path + project_name + "/"
     if os.path.isdir(project_path):
         print("ERROR: {} already exists in this directory".format(project_name))
         exit(1)
