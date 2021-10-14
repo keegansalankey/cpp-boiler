d45ad85e781ab6e7f3f54f78e70839f50662f9a3
Keegan Salankey <keegansalankey@gmail.com>
2021-10-14 04:26:19 -0600

    CMake is more functional, copying complete!

--- make_project.py
+++ make_project.py
@@ -2,12 +2,19 @@ import sys
 import os
 import re
 import shutil
+import fileinput
+
+def insert_project_name(name, location):
+    with fileinput.FileInput(location, inplace=True, backup='.bak') as file:
+        for line in file:
+            print(line.replace('<<replace_me>>', name), end='')
+            os.remove(location + ".bak")
 
 if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv)
     
-    # Add function to print usage instructions at some point 
+    # TODO: print usage instructions at some point 
     if (argc != 3):
         print("ERROR: Please enter two arguments")
         exit(1)
@@ -15,27 +22,31 @@ if __name__ == "__main__":
     template_type, project_name = argv[1], argv[2]
     
     ####################################################################################################
-    #
-    # Thoughts on how to make this work outside of the cpp-boiler directory:
-    # 
-    #   1) Store working directory in variable to know where to create project
-    #   2) Get home directory path and then append path to the cpp-boiler directory
-    #   3) Change directory to home directory
-    #   3) Should be able to copy between those two
-    #
+    #                                                                                                  # 
+    # Thoughts on how to make this work outside of the cpp-boiler directory:                           #
+    #                                                                                                  #
+    #   1) Store working directory in variable to know where to create project                         #
+    #   2) Get home directory path and then append path to the cpp-b:oiler directory                   #
+    #   3) Change directory to home directory                                                          #
+    #   3) Should be able to copy between those two                                                    #
+    #                                                                                                  #
+    # NOTE: This issue is resolved, but I will leave this here for future understanding                #
+    #                                                                                                  #
     ####################################################################################################
-    
+   
+    # Getting path of this file as it is executed
     boiler_path = os.path.dirname(os.path.realpath(__file__))
+
+    # Getting path to working directory where execution is happening
     target_path = os.getcwd() + '/'
 
-    # verify that template type exists by scanning through directory
+    # verify that template type exists by scanning through directory and append '/' if so
     with os.scandir(boiler_path + "/templates/") as templates:
         for template in templates:
             if template.is_dir() and template.name == template_type:
                 template_type += "/"
-                print(template_type)
     
-    # usig regex to check if template_type was appended
+    # using regex to check if template_type was appended
     check = re.compile('\w*/')
     if not check.match(template_type):
         print("ERROR: {} is not an existing template type".format(template_type))
@@ -44,8 +55,21 @@ if __name__ == "__main__":
     
     template_path = boiler_path + "/templates/" + template_type
     project_path = target_path + project_name + "/"
+    
+    # Checking if project is already to made to prevent overwriting
     if os.path.isdir(project_path):
         print("ERROR: {} already exists in this directory".format(project_name))
         exit(1)
 
     shutil.copytree(template_path, project_path)
+    
+    insert_project_name(project_name, project_path + "CMakeLists.txt")
+    #if os.path.exists(project_path + "CMakeLists.txt.bak"):
+     #   os.remove(project_path + "CMakeLists.txt.bak")
+
+    insert_project_name(project_name, project_path + "build.sh")
+   # if os.path.exists(project_path + "build.sh.bak"):
+        #os.remove(project_path + "build.sh.bak")
+
+    #if os.path.exists(project_path + "build/"):
+        #shutil.rmtree(project_path + "build/")
